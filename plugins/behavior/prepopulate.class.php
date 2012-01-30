ffc00cdc62e533f5a46000e9fa451c9b01f1345e
Amitaibu <amitai-git@gizra.com>
2012-01-30 23:38:37 +0200

    Implement "redirect" action.

--- plugins/behavior/prepopulate.class.php
+++ plugins/behavior/prepopulate.class.php
@@ -23,16 +23,8 @@ class EntityReferencePrepopulateInstanceBehavior extends EntityReference_Behavio
         'none' => t('Do nothing'),
         'hide' => t('Hide field'),
         'form_error' => t('Set form error'),
-        'replace_widget' => t('Replace widget'),
         'redirect' => t('Redirect'),
       ),
-    );
-    $form['widget'] = array(
-      '#type' => 'select',
-      '#title' => t('Widget'),
-      '#description' => t('Determine what widget is used when using fallback.'),
-      '#options' => array(),
-      '#default_value' => isset($field['settings']['handler_settings']['widget']) ? $field['settings']['handler_settings']['widget'] : 'none',
     );
     return $form;
   }
